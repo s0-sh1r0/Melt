32604169a70f66e5655599eb9f0f0516df7e648e
s0-sh1r0 <sou.create04@gmail.com>
2025-09-08 22:15:36 +0900

    Initial Commit

---
+++ Melt/MeltApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeltApp.swift
+//  Melt
+//
+//  Created by 牛田壮志郎 on 2025/09/08.
+//
+
+import SwiftUI
+
+@main
+struct MeltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
